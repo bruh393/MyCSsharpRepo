d32913ca902404c9c418b0b5fd77d19ecd400064
bruh393 <shashwatmallick335@gmail.com>
2025-12-07 20:35:16 +0530

    Update Program.cs

--- GitTest/Program.cs
+++ GitTest/Program.cs
@@ -5,6 +5,12 @@
         int a = 3;
         Console.WriteLine("meth" + a);
         string s = Console.ReadLine();
-        
+        Test t = new Test();
+        t.Prin(33);
+    }
+    public void Prin(int a)
+    {
+        int p = a + 40;
+        Console.WriteLine(p);
     }
 }
